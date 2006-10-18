613b73f66469141c03e07697cc761cd16ad33be1
Timothy Lee Styer <tlstyer@gmail.com>
2006-10-18 08:32:37 +0000

    do my own layout for the MainFrame

--- source/com/tlstyer/netacquireclient/MainFrame.java
+++ source/com/tlstyer/netacquireclient/MainFrame.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 
 public class MainFrame implements ActionListener, ComponentListener {
 	private JFrame frame;
-	private GridLayout gl;
 	private JPanel panel;
 	
     private GameBoard board;
@@ -30,13 +29,15 @@ public class MainFrame implements ActionListener, ComponentListener {
     	scoresheet = new ScoreSheet(frame.getBackground());
     	
     	// layout the components
-        gl = new GridLayout(1, 2, 2, 2);
-        panel = new JPanel(gl);
+        panel = new JPanel(null);
 		panel.add(board.getPanel());
 		panel.add(scoresheet.getPanel());
+
+		// don't know what to call these!
+		panel.addComponentListener(this);
+        frame.getContentPane().add(panel, BorderLayout.CENTER);
 		
         //Display the window.
-        frame.getContentPane().add(panel, BorderLayout.CENTER);
         frame.pack();
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setVisible(true);
@@ -52,6 +53,40 @@ public class MainFrame implements ActionListener, ComponentListener {
     }
     
     public void componentResized(ComponentEvent e) {
+        int bw = 4;
+        int bw_div_2 = bw / 2;
+        int bw_plus_bw_div_2 = bw + bw_div_2;
+
+        int w = panel.getWidth();
+        int h = panel.getHeight();
+
+        int GBw = w / 2 - bw_plus_bw_div_2;
+        int GBh = GBw * 3 / 4;
+        board.getPanel().setBounds(bw, bw, GBw, GBh);
+        board.getPanel().validate();
+
+//		TRBy = bw + GBh + bw
+//		TRBh = GBh * 2 / 13
+//		self.tile_rack_bg.SetDimensions(bw, TRBy, GBw, TRBh)
+
+//		TRh = TRBh * 8 / 10
+//		TRw = TRh * 6 + AcquireTileRack.spacing * 5
+//		TRx = (w / 2 + bw_div_2 - TRw) / 2
+//		TRy = TRBy + (TRBh - TRh) / 2
+//		self.tile_rack.SetDimension(TRx, TRy, TRw, TRh)
+
+//		Ly = TRBy + TRBh + bw
+//		Lh = h - bw - Ly
+//		self.lobby.SetDimensions(bw, Ly, GBw, Lh)
+
+		int SSx = w / 2 + bw_div_2;
+		int SSh = GBw * 10 / 18;
+		scoresheet.getPanel().setBounds(SSx, bw, GBw, SSh);
+		scoresheet.getPanel().validate();
+
+//		GRy = bw + SSh + bw
+//		GRh = h - bw - GRy
+//		self.game_room.SetDimensions(SSx, GRy, GBw, GRh)
     }
     
     public void componentShown(ComponentEvent e) {
