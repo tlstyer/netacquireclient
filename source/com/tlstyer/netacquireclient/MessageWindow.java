0bc867a6ab39359bea9e2481b916f60a2fc9adde
Timothy Lee Styer <tlstyer@gmail.com>
2006-10-22 04:33:49 +0000

    MessageWindow: has scrollbar, scrolls to bottom after appending

--- source/com/tlstyer/netacquireclient/MessageWindow.java
+++ source/com/tlstyer/netacquireclient/MessageWindow.java
@@ -1,12 +1,22 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class MessageWindow extends JTextArea {
+public class MessageWindow extends JScrollPane {
+	private JTextArea jtextarea = new JTextArea();
+	
 	private static final Color colorbg = new Color(192, 192, 255);
 	
 	public MessageWindow() {
-		setBackground(colorbg);
-		setEditable(false);
-		this.setLineWrap(true);
+		jtextarea.setEditable(false);
+		jtextarea.setLineWrap(true);
+		jtextarea.setBackground(colorbg);
+		setViewportView(jtextarea);
+		setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+	}
+	
+	public void append(String str) {
+		jtextarea.append(str);
+		jtextarea.setCaretPosition(jtextarea.getDocument().getLength());
 	}
 }
