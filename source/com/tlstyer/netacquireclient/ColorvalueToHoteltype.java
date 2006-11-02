0003322190ca8ed4779f3a1504f5d62f2f5fbd0e
Timothy Lee Styer <tlstyer@gmail.com>
2006-11-02 08:18:56 +0000

    ColorvalueToHoteltype: initialize hashmap at startup

--- source/com/tlstyer/netacquireclient/ColorvalueToHoteltype.java
+++ source/com/tlstyer/netacquireclient/ColorvalueToHoteltype.java
@@ -2,9 +2,7 @@ import java.util.*;
 
 public class ColorvalueToHoteltype {
 	private static final Map<Integer, Integer> hashmap = new HashMap<Integer, Integer>();
-	private static boolean isInitialized = false;
-	
-	private static void initialize() {
+    static {
         hashmap.put(12648447, BoardtypeEnum.BOARDTYPE_NONE.ordinal());
         hashmap.put(255, BoardtypeEnum.BOARDTYPE_LUXOR.ordinal());
         hashmap.put(65535, BoardtypeEnum.BOARDTYPE_TOWER.ordinal());
@@ -20,14 +18,9 @@ public class ColorvalueToHoteltype {
         hashmap.put(12648384, BoardtypeEnum.BOARDTYPE_HAVE_NEIGHBORING_TILE_TOO.ordinal());
         hashmap.put(16777215, BoardtypeEnum.BOARDTYPE_WILL_FORM_NEW_CHAIN.ordinal());
         hashmap.put(6316128, BoardtypeEnum.BOARDTYPE_CANT_PLAY_NOW.ordinal());
-
-		isInitialized = true;
 	}
 
 	public static int lookup(int colorvalue) {
-		if (!isInitialized) {
-			initialize();
-		}
 		return hashmap.get(colorvalue);
 	}
 }
