db1eb3cbc226c29c3e5cda1c13e4b03185ef51b1
Timothy Lee Styer <tlstyer@gmail.com>
2013-10-27 22:13:50 -0700

    add Permissions attribute to the manifest file. update release file. update netbeans stuff.

--- release.sh
+++ release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-java -jar ~/proguard4.6/lib/proguard.jar @proguard.pro 
+java -jar ~/proguard4.10/lib/proguard.jar @proguard.pro
 
 jarsigner -storepass password -keypass password -signedjar dist/NetAcquireClient.jar dist/proguard.jar mykey
 
-cp dist/NetAcquireClient.jar ~/projects/tlstyer.com/NetAcquireClient/
+cp dist/NetAcquireClient.jar ~/code/tlstyer.com/NetAcquireClient/
 
